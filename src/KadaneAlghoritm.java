0f85d69a8e28222de5c61f5a89ffefcfc2841711
Ruza Isembekova <253458@astanait.edu.kz>
2026-04-07 11:01:59 +0300

    1

---
+++ src/KadaneAlghoritm.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class KadaneAlghoritm {
+    public static int maxSubArraySum(ArrayList<Integer> list) {
+        if (list == null || list.isEmpty()) {
+            return 0;
+        }
+
+        int maxCurrent = list.get(0);
+        int maxGlobal = list.get(0);
+
+        for (int i = 1; i < list.size(); i++) {
+            maxCurrent = Math.max(list.get(i), maxCurrent + list.get(i));
+            if (maxCurrent > maxGlobal) {
+                maxGlobal = maxCurrent;
+            }
+        }
+        return maxGlobal;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println("Enter " + n + " integers:");
+
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        int result = maxSubArraySum(list);
+        System.out.println("Maximum contiguous subarray sum = " + result);
+
+        sc.close();
+
+    }
+}
